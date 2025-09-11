2c74e774fe87aaa9a9efc1d4767a17b942b3b1dc
Hussain00Ansari <hussainansarib799@gmail.com>
2025-09-11 10:31:13 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,8 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",   // 👈 ensures assets load correctly on Netlify
+  base: "./",  // 👈 important for Netlify
   plugins: [react(), tailwindcss()],
 })
